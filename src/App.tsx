39c6ca0ba4c43682347e952fafd8740acce3dcc5
Lucas Giraldelli <vinicius.giraldelli@hotmail.com>
2023-07-21 17:11:26 -0400

    fix: unused var

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/Login';
 
-function App({ routes }) {
+function App() {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
