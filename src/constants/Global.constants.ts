15f72379ce7b01fa276eb241e962e9d7acf3c4b7
Lucas Giraldelli <vinicius.giraldelli@hotmail.com>
2024-02-03 21:42:47 -0400

    fix: mistranslations

--- src/constants/Global.constants.ts
+++ src/constants/Global.constants.ts
@@ -2,15 +2,15 @@ export const MAIN_TITLE =
   'INSTITUTO DA CRIANÇA - ESCOLA DE EVANGELIZAÇÃO ESPÍRITA INFANTIL';
 export const STUDENT_TABLE_TITLE = 'Listagem de alunos';
 
-export const LABEL_DISPLAYED_ROWS = '{from}-{to} of {count}';
-export const LABEL_ROWS_PER_PAGE = 'Rows per page';
-export const LABEL_ROWS_SELECT = 'Row(s)';
+export const LABEL_DISPLAYED_ROWS = '{from}-{to} de {count}';
+export const LABEL_ROWS_PER_PAGE = 'Linhas por página';
+export const LABEL_ROWS_SELECT = 'Linhas(s)';
 
-export const N_ROWS_SELECTED = '{0} row(s) selected';
-export const SEARCH_TOOLTIP = 'Enter something';
-export const SEARCH_PLACEHOLDER = 'Search for information';
+export const N_ROWS_SELECTED = '{0} Linhas(s) selecionadas';
+export const SEARCH_TOOLTIP = 'Digite algo';
+export const SEARCH_PLACEHOLDER = 'Procurar informação';
 
 export const HEADER_ACTIONS = 'Ações';
 
-export const EMPTY_DATASOURCE_MESSAGE = 'No data to show';
-export const FILTER_ROW_TOOLTIP = 'Filter';
+export const EMPTY_DATASOURCE_MESSAGE = 'Nenhum dado para mostrar';
+export const FILTER_ROW_TOOLTIP = 'Filtrar';
